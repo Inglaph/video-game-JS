a54af68df48f03905993ffeabe3ecfddce5e6614
inglap <inglaph@gmail.com>
2023-08-20 20:52:18 +0100

    funcion de renderizado de mapas con map.js version lap

--- game.js
+++ game.js
@@ -1,47 +1,68 @@
 const $canvas = document.querySelector('#game');
 const game = $canvas.getContext('2d')
+let arr = maps
+let level = 1
+
 console.log(game);
 
-// lap: tools -------------------------------------------------------------------------//
-
-
-
-
-
-// lap: create a function to return the canvas size for square form
+// lap: create a function to return the canvas size for square form 
 function canvasSizeSquare() {
-    // console.log('tamañoPantalla an * al => ' + window.innerWidth + ' * ' + window.innerHeight) // lap: probe
     let heightScreen = window.innerHeight
     let widthScreen = window.innerWidth
     return Math.min(heightScreen, widthScreen)
 }
 
+// lap: create a function to verify the array the map es square
+function validationArrayMapSquare(arr, level) {
+    let arrMap = arr.find(element => element.level === level)
+    let verifyRows = arrMap.map.length
+    let vefResult = true
+    arrMap.map.forEach((element, index) => {
+        for (let vefC = 0; vefC < element.length; vefC++) {
+            if (element[vefC].length != verifyRows) {
+                vefResult = false
+            }
+        }
+    });
 
-window.addEventListener('load', startGame)
-window.addEventListener('resize', startGame)
+    if (vefResult === false) {
+        return false
+    } else {
+        return arrMap.map
+    }
+}
 
-function startGame() {
-    let canvasSize = canvasSizeSquare()
+// window.addEventListener('load', startGame(arr, level)) // <-- LOAD
+window.addEventListener('resize', startGame(arr, level))
 
+// lap: create a function to start the game ------------------------->
+function startGame(arr, level) {
+    // lap: information about canvas size
+    let canvasSize = canvasSizeSquare(arr, level)
     $canvas.setAttribute('width', canvasSize * 0.75)
     $canvas.setAttribute('height', canvasSize * 0.75)
 
-    let matrix = 11
-    let elementSize = (canvasSize / matrix)
-    // console.log('elementSize: ' + elementSize) // lap: probe
+    // lap: verify matrix square
+    let map = validationArrayMapSquare(arr, level)
+    if (map === false) {
+        return console.warn('Error: matrix square')
+    }
+
+    // lap: create of the map game acording to the arr received
+    let elementSize = (canvasSize / map.length)
     let renElementSize = elementSize - LapTools.findValPercentage(26.726, elementSize)
     let positionC = renElementSize
     let positionR = renElementSize
-    // console.log('renElementSize: ' + Number(renElementSize.toFixed(1)), 'canvasSize: ' + canvasSize) // lap: probe
     game.font = renElementSize + 'px Arial'
     game.textAlign = 'right'
-    for (let r = 0; r < matrix; r++) {
-        for (let c = 0; c < matrix; c++) {
-            game.fillText(emojis['X'], positionC + LapTools.findValPercentage(0.556, canvasSize), positionR)
+    for (row in map) {
+        let lineMap = map[row]
+        lineMap = lineMap[0].split('')
+        for (let c = 0; c < lineMap.length; c++) {
+            game.fillText(emojis[lineMap[c]], positionC + LapTools.findValPercentage(0.556, canvasSize), positionR)
             positionC += renElementSize
         }
         positionC = renElementSize
         positionR += renElementSize
     }
-
 }
--- maps.js
+++ maps.js
@@ -4,50 +4,60 @@
 */
 
 const emojis = {
-    '-': ' ',
-    'O': '🚪',
-    'X': '💣',
-    'I': '🎁',
-    'PLAYER': '💀',
-    'BOMB_COLLISION': '🔥',
-    'GAME_OVER': '👎',
-    'WIN': '🏆',
+  '-': ' ',
+  'O': '🚪',
+  'X': '💣',
+  'I': '🎁',
+  'PLAYER': '💀',
+  'BOMB_COLLISION': '🔥',
+  'GAME_OVER': '👎',
+  'WIN': '🏆',
 };
 
 const maps = [];
-maps.push(`
-    IXXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    -XXXXXXXXX
-    OXXXXXXXXX
-  `);
-maps.push(`
-    O--XXXXXXX
-    X--XXXXXXX
-    XX----XXXX
-    X--XX-XXXX
-    X-XXX--XXX
-    X-XXXX-XXX
-    XX--XX--XX
-    XX--XXX-XX
-    XXXX---IXX
-    XXXXXXXXXX
-    `);
-maps.push(`
-    I-----XXXX
-    XXXXX-XXXX
-    XX----XXXX
-    XX-XXXXXXX
-    XX-----XXX
-    XXXXXX-XXX
-    XX-----XXX
-    XX-XXXXXXX
-    XX-----OXX
-    XXXXXXXXXX
-  `);
+maps.push({
+  level: 0,
+  map: [
+    ['IXXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['-XXXXXXXXX'],
+    ['IXXXXXXXXX']
+  ]
+}
+);
+maps.push({
+  level: 1,
+  map: [
+    ['O--XXXXXXX'],
+    ['X--XXXXXXX'],
+    ['XX----XXXX'],
+    ['X--XX-XXXX'],
+    ['X-XXX--XXX'],
+    ['X-XXXX-XXX'],
+    ['XX--XX--XX'],
+    ['XX--XXX-XX'],
+    ['XXXX---IXX'],
+    ['XXXXXXXXXX']
+  ]
+});
+maps.push({
+  level: 2,
+  map: [
+    ['I-----XXXX'],
+    ['XXXXX-XXXX'],
+    ['XX----XXXX'],
+    ['XX-XXXXXXX'],
+    ['XX-----XXX'],
+    ['XXXXXX-XXX'],
+    ['XX-----XXX'],
+    ['XX-XXXXXXX'],
+    ['XX-----OXX'],
+    ['XXXXXXXXXX']
+  ]
+});
